9bff65f82fde8471e46cfc6fb64376f96bcc9a6f
roimanromero <roimanromerorobinson@gmail.com>
2025-03-29 20:40:54 -0500

    database changes

--- database/new assignment2.sql
+++ database/new assignment2.sql
@@ -230,3 +230,9 @@ VALUES   (
     'White',
     5
   );
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
