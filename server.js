8dcf0aaa7d463869c410929afbe322fdd01019b4
roimanromero <roimanromerorobinson@gmail.com>
2025-04-05 17:23:27 -0500

    please

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const utilities = require("./utilities/")
+
+
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -24,27 +24,9 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get("/", baseController.buildHome)
-
-// File Not Found Route - must be last route in list
-app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
 })
-
-/* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
-app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  res.render("errors/error", {
-    title: err.status || 'Server Error',
-    message: err.message,
-    nav
-  })
-})
-
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
