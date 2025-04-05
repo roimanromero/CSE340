d8de67880590a1d88d0604bab8d5045f3653150f
roimanromero <roimanromerorobinson@gmail.com>
2025-04-05 09:04:44 -0500

    new tony starrk data

--- database/new assignment2.sql
+++ database/new assignment2.sql
@@ -230,6 +230,28 @@ VALUES   (
     'White',
     5
   );
+--Assignment 2 data
+--Insert new record into the account table
+INSERT INTO public.account (
+  account_firstname,
+  account_lastname,
+  account_email,
+  account_password
+)
+VALUES (
+  'Tony',
+  'Stark',
+  'tony@starkent.com',
+  'Iam1ronM@n'
+);
+--Modify the account type for the new record
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+--Delete the record from the account table
+DELETE FROM public.account
+WHERE account_email = 'tony@starkent.com';
+
 UPDATE public.inventory
 SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
 WHERE inv_make = 'GM' AND inv_model = 'Hummer';
